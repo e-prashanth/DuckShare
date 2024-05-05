99976fdf7857b942bdf685504cde4ee9e2bc6ff2
Prashanth <130282405+e-prashanth@users.noreply.github.com>
2024-05-05 06:22:38 +0000

    done12

--- app.py
+++ app.py
@@ -98,5 +98,5 @@ def getFiles(fileData):
     return render_template("aftercode.html",fileData['fileName'])
 
 
-if __name__ == __main__:
+if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
